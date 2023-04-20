6ad09ee9e5e1165f2f543d459fceb69d93393500
Mariam Garcia <mgxbreu@DESKTOP-029S51T.localdomain>
2023-04-19 21:12:12 -0400

    ✨ use classes

--- main.py
+++ main.py
@@ -1,4 +1,10 @@
-import json
-from azure.core.exceptions import ResourceNotFoundError
-from azure.core.credentials import AzureKeyCredential
-from azure.ai.formrecognizer import FormRecognizerClient, FormTrainingClient
+from classes.recognize_invoice import RecognizeInvoice
+from classes.save_to_csv import SaveToCsv
+
+if __name__ == '__main__':
+    sample = RecognizeInvoice()
+    sample.recognize_invoice()
+    info = sample.get_info()
+    save = SaveToCsv()
+    save.save_to_csv(info)
+    print(info)
