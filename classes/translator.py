84e2f82610ae09da140bdd9efaeae754af5f0a9c
Mariam Garcia <mgxbreu@DESKTOP-029S51T.localdomain>
2023-04-19 23:28:50 -0400

    ✨ add class to translate

---
+++ classes/translator.py
@@ -0,0 +1,51 @@
+from utils import get_credentials
+
+import os, requests, uuid, json
+
+KEY, ENDPOINT = get_credentials("translator")
+region = 'eastus'
+
+def detect_language(text):
+    path = '/detect?api-version=3.0'
+    constructed_url = ENDPOINT + path
+
+    headers = {
+        'Ocp-Apim-Subscription-Key': KEY,
+        'Ocp-Apim-Subscription-Region': region,
+        'Content-type': 'application/json',
+        'X-ClientTraceId': str(uuid.uuid4())
+    }
+    body = [{
+        'text': text
+    },]
+    request = requests.post(constructed_url, headers=headers, json=body)
+    response = request.json()
+    response = response[0]
+    language = response['language']
+    return language
+
+def translate(text, language):
+    path = '/translate?api-version=3.0'
+    params = f'&from={language}&to=es'
+    constructed_url = ENDPOINT + path + params
+
+    headers = {
+        'Ocp-Apim-Subscription-Key': KEY,
+        'Ocp-Apim-Subscription-Region': region,
+        'Content-type': 'application/json',
+        'X-ClientTraceId': str(uuid.uuid4())
+    }
+
+    # You can pass more than one object in body.
+    body = [{
+        'text' : text
+    }]
+    request = requests.post(constructed_url, headers=headers, json=body)
+    response = request.json()
+    response = response[0]
+    response = response['translations']
+    response = response[0]
+    response = response['text']
+    return response
+
+# print(json.dumps(response, sort_keys=True, indent=4, ensure_ascii=False, separators=(',', ': ')))
