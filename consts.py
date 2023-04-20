783e594ba4400a4794e885d7c6a57f439adbd36e
Mariam Garcia <mgxbreu@DESKTOP-029S51T.localdomain>
2023-04-19 23:27:20 -0400

    🚧 add headers

---
+++ consts.py
@@ -0,0 +1,15 @@
+headers = [
+    "file_name",
+    "business_name",
+    "business_address",
+    "country",
+    "address",
+    "date",
+    "time",
+    "payment_method",
+    "total",
+    "product_quantity",
+    "subtotal",
+    "tax",
+    "product_type",
+]
