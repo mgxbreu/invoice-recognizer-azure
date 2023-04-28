9ff98dea19938f1cb4c45e5975cfefb23ec89d48
Mariam Garcia <20163810@itla.edu.do>
2023-04-27 22:02:41 -0400

    ✏️ change import location

--- main.py
+++ main.py
@@ -1,10 +1,8 @@
 from classes.save_to_csv import SaveToCsv
-from utils.file_processing import FileProcessing
+from classes.file_processing import FileProcessing
 
 
 if __name__ == '__main__':
     directory = 'traininghihi'
     process = FileProcessing()
     processed_data = process.process_directory(directory)
-    save = SaveToCsv()
-    save.save_to_csv(processed_data)
