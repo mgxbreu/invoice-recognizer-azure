32e8d048a134af3e01f97b4d91c79791973991e2
Mariam Garcia <20163810@itla.edu.do>
2023-04-28 17:07:17 -0400

    🚚 move headers to consts

--- classes/translator.py
+++ classes/translator.py
@@ -1,23 +1,15 @@
-from utils.credentials import get_credentials
+from consts import service_headers, ENDPOINT
 import os, requests, uuid, json
 
-KEY, ENDPOINT = get_credentials("translator")
-region = 'eastus'
 
 def detect_language(text):
     path = '/detect?api-version=3.0'
     constructed_url = ENDPOINT + path
 
-    headers = {
-        'Ocp-Apim-Subscription-Key': KEY,
-        'Ocp-Apim-Subscription-Region': region,
-        'Content-type': 'application/json',
-        'X-ClientTraceId': str(uuid.uuid4())
-    }
     body = [{
         'text': text
     },]
-    request = requests.post(constructed_url, headers=headers, json=body)
+    request = requests.post(constructed_url, headers=service_headers, json=body)
     response = request.json()
     response = response[0]
     language = response['language']
@@ -28,18 +20,11 @@ def translate(text, language):
     params = f'&from={language}&to=es'
     constructed_url = ENDPOINT + path + params
 
-    headers = {
-        'Ocp-Apim-Subscription-Key': KEY,
-        'Ocp-Apim-Subscription-Region': region,
-        'Content-type': 'application/json',
-        'X-ClientTraceId': str(uuid.uuid4())
-    }
-
     # You can pass more than one object in body.
     body = [{
         'text' : text
     }]
-    request = requests.post(constructed_url, headers=headers, json=body)
+    request = requests.post(constructed_url, headers=service_headers, json=body)
     response = request.json()
     response = response[0]
     response = response['translations']
--- consts.py
+++ consts.py
@@ -1,3 +1,9 @@
+from utils.credentials import get_credentials
+import uuid
+
+KEY, ENDPOINT = get_credentials("translator")
+region = 'eastus'
+
 headers = [
     "file_name",
     "business_name",
@@ -13,3 +19,10 @@ headers = [
     "tax",
     "product_type",
 ]
+
+service_headers = {
+        'Ocp-Apim-Subscription-Key': KEY,
+        'Ocp-Apim-Subscription-Region': region,
+        'Content-type': 'application/json',
+        'X-ClientTraceId': str(uuid.uuid4())
+    }
